0705c99852064db12b60871566612cc0d828a7ad
harshal <kardileharshal567@gmail.com>
2024-04-25 13:54:10 +0530

    initialized RESTApis

---
+++ banking-app/src/main/java/com/example/banking/service/impl/AccountServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.banking.service.impl;
+
+import com.example.banking.dto.AccountDto;
+import com.example.banking.entity.Account;
+import com.example.banking.mapper.AccountMapper;
+import com.example.banking.repository.AccountRepository;
+import com.example.banking.service.AccountService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    private AccountRepository accountRepository;
+
+    public AccountServiceImpl(AccountRepository accountRepository){
+            this.accountRepository = accountRepository;
+    }
+    @Override
+    public AccountDto createAccount(AccountDto accountDto) {
+
+        Account account= AccountMapper.mapToAccount(accountDto);
+        Account savedAccount = accountRepository.save(account);
+
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
+}
