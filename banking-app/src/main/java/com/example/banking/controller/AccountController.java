98df2d55d3556da2de731142701f5c46b3c1b419
harshal <kardileharshal567@gmail.com>
2024-04-26 18:03:10 +0530

    updated

--- banking-app/src/main/java/com/example/banking/controller/AccountController.java
+++ banking-app/src/main/java/com/example/banking/controller/AccountController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
-    private AccountService accountService;
+    private final AccountService accountService;
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
